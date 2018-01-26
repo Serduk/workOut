6962bc2b6ca39c43f100e5a5d42a48d647475593
Serdyuk <serduksergey90@gmail.com>
2018-01-26 15:20:51 +0200

    add switching between screen size of device. TODO: should be added switcher for size detection

---
+++ app/src/main/java/com/example/sserdiuk/workout/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.example.sserdiuk.workout;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailActivity extends AppCompatActivity {
+    public static final String EXTRA_WORKOUT_ID = "id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        WorkoutDetailFragment fragment = (WorkoutDetailFragment) getFragmentManager().
+                findFragmentById(R.id.detail_frag);
+
+        int workoutID = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+        fragment.setWorkout(workoutID);
+    }
+
+}
