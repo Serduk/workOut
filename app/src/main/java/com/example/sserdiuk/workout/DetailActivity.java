e4e1f3288931962f1c7d8c44360ca85443ba27a4
Serhii Serdiuk <sserdiuk@kvp1403001.lohika.com>
2018-01-29 17:42:45 +0200

    cleanup code

--- app/src/main/java/com/example/sserdiuk/workout/DetailActivity.java
+++ app/src/main/java/com/example/sserdiuk/workout/DetailActivity.java
@@ -1,11 +1,7 @@
 package com.example.sserdiuk.workout;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_WORKOUT_ID = "id";
